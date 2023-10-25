a06910e0f4125240d0fea4e6243d3f4fe37fe0d6
sabin456 <148375032+sabin456@users.noreply.github.com>
2023-10-24 22:34:08 -0400

    homepage update

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-function openTutorialModal() {
-    const modal = document.getElementById('tutorialModal');
-    modal.style.display = 'block';
-}
-
-function closeTutorialModal() {
-    const modal = document.getElementById('tutorialModal');
-    modal.style.display = 'none';
-}
