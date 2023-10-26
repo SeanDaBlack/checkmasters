afbf3c5f682092dc43111eb095585b698473e378
amart247 <123605167+amart247@users.noreply.github.com>
2023-10-26 19:40:02 -0400

    updated js

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+function openTutorialModal() {
+    const modal = document.getElementById('tutorialModal');
+    modal.style.display = 'block';
+}
+
+function closeTutorialModal() {
+    const modal = document.getElementById('tutorialModal');
+    modal.style.display = 'none';
+}
