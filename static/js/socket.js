fb847a19d593a8716c4c69f860f2ba6b3217f538
Sean <seandablack@gmail.com>
2023-11-03 15:54:46 -0400

    added chat & sockets

---
+++ static/js/socket.js
@@ -0,0 +1,41 @@
+
+
+// spectate functionality
+function startSocket() {
+    socket = io({ autoConnect: false });
+    return socket
+}
+// document.getElementById("start").addEventListener("click", function () {
+//     const username = document.getElementById("username").value;
+//     socket.connect();
+//     socket.on('connect', function () {
+//         socket.emit('user_join', username);
+//     });
+//     document.getElementById("landing").style.display = "none";
+//     document.getElementById("game").style.display = "block";
+// });
+
+
+document.getElementById("send").addEventListener("click", function(){
+    const val = document.getElementById("chat-box").value;
+    const hidden_name = document.getElementById("hidden-name").value;
+    const room = document.getElementById("hidden-room").value;
+
+    const data = hidden_name + ": " + val
+    socket.emit("room_message", {"data":data, "room":room})
+    document.getElementById("chat-box").value = "";
+})
+
+document.getElementById("send").addEventListener("keyup", function(event){
+
+    if (event.key == "Enter") {
+        const val = document.getElementById("chat-box").value;
+        const hidden_name = document.getElementById("hidden-name").value;
+        const room = document.getElementById("hidden-room").value;
+
+        const data = hidden_name + ": " + val
+        socket.emit("room_message", {"data":data, "lobby_name":room})
+        document.getElementById("chat-box").value = "";
+    }
+})
+
